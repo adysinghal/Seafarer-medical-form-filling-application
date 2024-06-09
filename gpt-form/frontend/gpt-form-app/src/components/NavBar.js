df9d8f0611bdfb8f03f239aee7d205564edbf0c4
Aditya Singhal <singhal.adityaa@gmail.com>
2024-06-09 16:20:10 +0530

    healthchek logo showing

--- gpt-form/frontend/gpt-form-app/src/components/NavBar.js
+++ gpt-form/frontend/gpt-form-app/src/components/NavBar.js
@@ -1,17 +1,17 @@
 import React from "react";
+import logo from '../healthchek.png'
 
 function NavBar() {
   return (
     <>
       <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid">
+        <div class="container-fluid mx-3">
           <a class="navbar-brand" href="#">
             <img
-              src=""
+              src={logo}
               alt="Adi"
-              width="30"
-              height="24"
-              class="d-inline-block align-text-top"
+              width="15%"              
+              class="d-inline-block mx-3"
             />
             Form Filling Application
           </a>
